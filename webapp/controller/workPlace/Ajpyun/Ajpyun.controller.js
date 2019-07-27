bece1753cbeea55582dbc6b4a82ef13c0d4b8b57
moohyun lim <lmh7sp@gmail.com>
2019-07-27 08:29:01 +0000

    연습 환경 구성

---
+++ webapp/controller/workPlace/Ajpyun/Ajpyun.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+	"sap/ui/study/git/controller/BaseController"
+], function (BaseController) {
+	"use strict";
+
+	return BaseController.extend("sap.ui.study.git.controller.workPlace.Ajpyun.Ajpyun", {
+		onInit: function () {
+
+		}
+	});
+	
+});
---
+++ webapp/controller/workPlace/Skeom/Skeom.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+	"sap/ui/study/git/controller/BaseController"
+], function (BaseController) {
+	"use strict";
+
+	return BaseController.extend("sap.ui.study.git.controller.workPlace.Skeom.Skeom", {
+		onInit: function () {
+
+		}
+	});
+	
+});
