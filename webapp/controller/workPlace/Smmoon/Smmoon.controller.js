3fb61603b058c70be3f948c46c206c760a005b52
Seonmin Moon <seonminmoon@gmail.com>
2019-09-24 05:29:27 +0000

    .

---
+++ webapp/controller/workPlace/Smmoon/Smmoon.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/study/git/controller/BaseController"
+], function (BaseController) {
+	"use strict";
+
+	return BaseController.extend("sap.ui.study.git.controller.workPlace.Smmoon.Smmoon", {
+		onInit: function () {
+
+		}
+	});
+});
